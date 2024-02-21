a35d3bc2b77a00d71e7b30ad6001b4d05e278c0c
Durgaprasad-MDP <121805491+Durgaprasad-MDP@users.noreply.github.com>
2024-02-21 12:08:39 +0530

    Delete force-app/main/default/lwc directory

--- force-app/main/default/lwc/bulma1/bulma1.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement } from 'lwc';
-import { loadStyle } from 'lightning/platformResourceLoader';
-import bulma1 from '@salesforce/resourceUrl/bulma1';
-export default class Bulma extends LightningElement {
-    connectedCallback() {
-        loadStyle(this, bulma1);
-    }
-}
